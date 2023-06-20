cdf659b6ef1af19d0a92061d68ec090eedae4b57
Legaspi Ricardo <108900203+LegaspiRicardo@users.noreply.github.com>
2023-06-20 10:47:51 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import './App.css';
+import './assets/css/App.css';
 
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
-import montana from './azul/montañass.png';
+import montana from './assets/images/azul/montañass.png';
 
-import montanaFondo from './azul/montañasFondo.png';
+import montanaFondo from './assets/images/azul/montañasFondo.png';
 
-import montanaMedioFondo1 from './azul/montañasMedioFondo1.png';
-import montanaMedioFondo1_1 from './azul/montañasMedioFondo1_1.png';
-import montanaMedioFondo2 from './azul/montañasMedioFondo2.png';
+import montanaMedioFondo1 from './assets/images/azul/montañasMedioFondo1.png';
+import montanaMedioFondo1_1 from './assets/images/azul/montañasMedioFondo1_1.png';
+import montanaMedioFondo2 from './assets/images/azul/montañasMedioFondo2.png';
 
-import peñascoIzq from './azul/peñascoIzquierdo1.png';
-import peñascoDer from './azul/peñascoDerecho1.png';
+import peñascoIzq from './assets/images/azul/peñascoIzquierdo1.png';
+import peñascoDer from './assets/images/azul/peñascoDerecho1.png';
 
-import montanaMedio1 from './azul/montañasMedio1.png';
-import montanaMedio2 from './azul/montañasMedio2.png';
-import montanaMedio3 from './azul/montañasMedio3.png';
+import montanaMedio1 from './assets/images/azul/montañasMedio1.png';
+import montanaMedio2 from './assets/images/azul/montañasMedio2.png';
+import montanaMedio3 from './assets/images/azul/montañasMedio3.png';
 
-import Prueba from './components/Prueba';
+//import Prueba from './components/Prueba';
 
 
 function App() {
@@ -50,7 +50,7 @@ function App() {
 
 
 
-      <Parallax pages={3.1} class='contenidoParalax' >
+      <Parallax pages={4.1} class='contenidoParalax' >
         {/*------------------ ESTRELLAS --------------*/}
         <ParallaxLayer
           offset={0}
@@ -205,6 +205,23 @@ function App() {
         </ParallaxLayer>
         {/*-------  ----------- Montañas medio fondo --------------*/}
         <ParallaxLayer
+          offset={2.3}
+          speed={0.2}
+          factor={2}
+          style={{
+            backgroundImage: `	url(${montanaFondo})`,
+            backgroundSize: 'contain',
+          }}
+        >
+        </ParallaxLayer>
+
+
+
+
+
+
+        {/*-------  ----------- Montañas medio fondo --------------*/}
+        <ParallaxLayer
           offset={1.8}
           speed={0.03}
           factor={1.4}
@@ -259,19 +276,6 @@ function App() {
           speed={0.05}
           factor={0.7}
         >
-          {/* <div class="container col-5 mx-auto carta_Intro">
-            <h3 className='col-md-10 mx-auto mb-4 tituloCartaIntro'>HTML and Bootstrap Section</h3>
-            <div className="row ">
-              <div className="col-md-10 mx-auto">
-                <div>
-                  <p className='textoCartaIntro'>
-                  Texto de relleno para generar muchos caracteres en este espacio de carta para hacer incluso una mini introducción como de istoria hacia la isstoriaThis is a Bootstrap alert in the additional section.
-                  Texto de relleno para generar muchos caracteres en este espacio de carta para hacer incluso una mini introducción como de istoria hacia la isstoriaThis is a Bootstrap alert in the additional section.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div> */}
 
 
 
@@ -283,6 +287,44 @@ function App() {
 
 
         </ParallaxLayer>
+
+
+
+        {/*------------------ Segunda Seccion--------------*/}
+        <ParallaxLayer
+          offset={2.8}
+          speed={0.07}
+          factor={4}
+        >
+          <div class="secondSection" > </div>
+        </ParallaxLayer>
+
+        {/*------------------ Texto Segunda Seccion--------------*/}
+        <ParallaxLayer
+          offset={3.1}
+          speed={0.3}
+          factor={4}
+        >
+          <div className='row'>
+            <div className='col-6'>
+
+            <h1 class="row col-10 mx-auto">Strange but true – there are 12 times more trees on Earth than stars in the Milky Way!</h1>
+            <p class="col-10 mx-auto">Scientists estimate there are between 200 – 400 billion stars in our galaxy while there are an estimated 1 trillion trees on Earth! Like the stars, trees live a long time and are truly important for life. Check out a related blog you might like .</p>
+
+
+            </div>
+
+            <div className='col-6'>
+
+            <h1 class="row col-10 mx-auto">Strange but true – there are 12 times more trees on Earth than stars in the Milky Way!</h1>
+            <p class="col-10 mx-auto">Scientists estimate there are between 200 – 400 billion stars in our galaxy while there are an estimated 1 trillion trees on Earth! Like the stars, trees live a long time and are truly important for life. Check out a related blog you might like .</p>
+
+
+            </div>
+
+          </div>
+        </ParallaxLayer>
+
 
 
       </Parallax>
@@ -301,4 +343,4 @@ function App() {
 
 }
 
-export default App;
+export default App; 
